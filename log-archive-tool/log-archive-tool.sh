7399489f4382b7c96e77a791792fa1a4f8060fc9
Catalin246 <avorniceseicatalin1234@gmail.com>
2024-12-09 23:23:18 +0100

    Update README.md and enhance log-archive-tool script with usage instructions and error handling

--- log-archive-tool/log-archive-tool.sh
+++ log-archive-tool/log-archive-tool.sh
@@ -1,3 +1,27 @@
-#! /usr/bin/bash
-echo $1
+#!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "USAGE: log-archive <log-dir>"
+    exit 1
+fi
+
+logs_dir="$1"
+
+if ! [ -d "$logs_dir" ]; then
+    echo "$logs_dir doesn't exist"
+    exit 1
+fi
+
+if ! [ -r "$logs_dir" ]; then
+    echo "Current user $USER has no permission to read the files in directory $logs_dir"
+    exit
+fi
+
+basename_dir=$(basename "$logs_dir")
+current_date=$(date +'%Y%m%d')
+current_time=$(date +'%H%M%S')
+logs_archive_name="$basename_dir"_archive_"$current_date"_"$current_time".tar.gz
+
+tar -czvf "$logs_archive_name" "$logs_dir"
+
+echo "The logs archive $logs_archive_name was created"
